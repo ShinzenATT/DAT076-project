1f6121f823843f4057cf7be675f64734d3152496
shinzenatt <42674003+ShinzenATT@users.noreply.github.com>
2023-02-17 00:18:38 +0100

    completed the basic home page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,63 @@
 <template>
-  <v-card style="background-image: url('/groupimg.jpg'); 
-                  background-size: cover;
-                  background-position: center center; 
-                  height: 25vh;">
-    <h1 style="text-align: center; color: white; position: relative; top: 45%;"> Igenjörsteknolog Sektionen Chalmers </h1>
-  </v-card>
+  <div>
+    <v-card flat style="background-image: url('/groupimg.jpg');
+                        background-size: cover;
+                        background-position: center center;
+                        height: 25vh;">
+      <h1 style="text-align: center;
+                  color: white;
+                  position: relative;
+                  top: 45%;
+                  text-shadow: #000 0 0 10px;"
+      > Ingejörsteknolog Sektionen Chalmers </h1>
+    </v-card>
+    <v-container style="text-align: center">
+      <h2> Välkommen till H-sektionen! </h2>
+      <p style="text-align: justify">
+        H-sektionen, även kallad ingenjörsteknologsektionen, är en sektion på Chalmers tekniska högskola för
+        högskoleingenjörsprogrammen för datateknik, design och produktutveckling, elektroteknik, ekonomi och
+        produktionsteknik, maskinteknik och mekatronik.
+      </p>
+      <h3> Läs mer om våra utbildningar här: </h3>
+      <v-btn flat stacked width="120" height="140"
+             href="https://www.chalmers.se/utbildning/hitta-program/datateknik-hogskoleingenjor/">
+        <v-img width="100" src="@/assets/data.png"/>
+        <b style="margin-top: 7px"> Data </b>
+      </v-btn>
+      <v-btn flat stacked style="width: 120px; height: 140px;"
+             href="https://www.chalmers.se/utbildning/hitta-program/elektroteknik-hogskoleingenjor/">
+        <v-img width="100" src="@/assets/elektro.png"/>
+        <b style="margin-top: 7px"> Elektro </b>
+      </v-btn>
+      <v-btn flat stacked style="width: 120px; height: 140px;"
+             href="https://www.chalmers.se/utbildning/hitta-program/mekatronik-hogskoleingenjor/">
+        <v-img width="100" src="@/assets/mekatronik.png"/>
+        <b style="margin-top: 7px"> Mekatronik </b>
+      </v-btn>
+      <v-btn flat stacked style="width: 120px; height: 140px;"
+             href="https://www.chalmers.se/utbildning/hitta-program/maskinteknik-hogskoleingenjor/">
+        <v-img width="100" src="@/assets/maskin.png"/>
+        <b style="margin-top: 7px"> Maskin </b>
+      </v-btn>
+      <v-btn flat stacked style="width: 120px; height: 140px;"
+             href="https://www.chalmers.se/utbildning/hitta-program/design-och-produktutveckling-hogskoleingenjor/">
+        <v-img width="100" src="@/assets/design.png"/>
+        <b style="margin-top: 7px"> Design </b>
+      </v-btn>
+      <v-btn flat stacked style="width: 120px; height: 140px;"
+             href="https://www.chalmers.se/utbildning/hitta-program/ekonomi-och-produktionsteknik-hogskoleingenjor/">
+        <v-img width="100" src="@/assets/epi.png"/>
+        <b style="margin-top: 7px"> EPI </b>
+      </v-btn>
+
+    </v-container>
+  </div>
 </template>
 
+<style>
+
+</style>
+
 <script lang="ts" setup>
-  
+
 </script>

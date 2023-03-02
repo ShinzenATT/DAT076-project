fc42dcdf32a180044abb33a9f0a1d7259df02965
Leobag <leonard.bagiu@gmail.com>
2023-03-02 17:54:09 +0100

    Dropdown menus nearly done

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -22,10 +22,35 @@
             class="d-flex justify-md-space-around"
             multiple>
 
-            <v-btn text large cols="auto">
-              Sektionen
-            </v-btn>
+            <div
+              class="d-flex justify-space-around"
+              v-for="current in routeArr" :key="current">
 
+              <v-menu>
+                <template v-slot:activator="{ props }">
+                  <v-btn
+                    v-bind="props"
+                  >
+                    {{current.title}}
+                  </v-btn>
+                </template>
+                <v-list>
+
+                  <v-list-item
+                    v-for="section of current.content" :key="section.link" :to="section.link"
+                  >
+
+                    <v-list-item-title>{{section.routeName}}</v-list-item-title>
+
+                  </v-list-item>
+
+                </v-list>
+              </v-menu>
+
+            </div>
+
+
+      <!--
             <v-btn text large cols="auto">
               Student
             </v-btn>
@@ -37,7 +62,7 @@
             <v-btn text large cols="auto">
               Kontakt
             </v-btn>
-
+       -->
 
           </v-btn-group>
 
@@ -48,7 +73,61 @@
 
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
 
+import {defineComponent} from "vue";
+
+  export default defineComponent({
+
+    data() {
+      return {
+
+        routeArr : {
+          item1: {
+
+            title: "Sektionen",
+            content: [
+              {routeName: 'Om H', link: '/om-h'},
+              {routeName: 'Styret', link: '/styret'},
+              {routeName: 'Dokument', link: '/dokument'},
+              {routeName: 'SAMO', link: '/SAMO'}
+            ]
+          },
+          item2: {
+            title: "Kommitteer",
+            content: [
+              {routeName: 'NollK', link: '/kommitteer/nollk'},
+              {routeName: 'DKV', link: '/kommitteer/dkv'},
+              {routeName: 'H6', link: '/kommitteer/h6'},
+              {routeName: 'pHoto', link: '/kommitteer/photo'},
+              {routeName: 'Rustet', link: '/kommitteer/rustet'},
+              {routeName: 'DSC', link: '/kommitteer/dsc'},
+              {routeName: 'HD', link: '/kommitteer/hd'},
+              {routeName: 'HKEX', link: '/kommitteer/hkex'},
+              {routeName: 'Lindholmsfestivalen', link: '/kommitteer/lindholmsfestivalen'},
+              {routeName: 'PubF', link: '/kommitteer/pubf'},
+              {routeName: 'SkHål', link: '/kommitteer/skhal'}
+            ]
+          },
+          item3: {
+            title: "Utskott",
+            content: [
+              {routeName: 'Studienämnden', link: '/utskott/snh'},
+              {routeName: 'Arbetsmarknadsutskottet', link: '/utskott/harm'},
+              {routeName: 'Jämlikhetsutskottet', link: '/utskott/jamh'}
+            ]
+          },
+          item4: {
+            title: "Kontakt",
+            content: [
+              {routeName: 'Programteam', link: '/programteam'},
+              {routeName: 'Akademihälsan', link: '/akademihalsan'}
+            ]
+          }
+        }
+      }
+    }
+
+  })
 
 </script>

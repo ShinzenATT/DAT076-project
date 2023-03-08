70c480408d4ebdc044b7f3fc2a31fddd49d2a2ff
shinzenatt <42674003+ShinzenATT@users.noreply.github.com>
2023-03-08 19:20:11 +0100

    made routers for accounts and committees

--- server/start.ts
+++ server/start.ts
@@ -1,7 +1,11 @@
 import express from "express";
-import { eventRouter } from "./router/eventRouter";
+import eventRouter from "./router/eventRouter";
+import accountRouter from "./router/accountRouter";
+import committeeRouter from "./router/committeeRouter";
 
 export const app = express();
 
 app.use(express.json());
 app.use("/event", eventRouter);
+app.use('/account', accountRouter);
+app.use('/committee', committeeRouter);

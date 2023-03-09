2d68624430b2b4b71464b67a025be905af879415
ShinzenATT <42674003+ShinzenATT@users.noreply.github.com>
2023-03-09 16:41:44 +0100

    Added committees page

---
+++ client/src/views/CommitteeInfo.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-container>
+
+    </v-container>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    name: 'committeeInfo',
+    data(){
+        return {
+            committee: {} as any
+        }
+    }
+})
+</script>
